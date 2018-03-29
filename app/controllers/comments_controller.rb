acb21d09c9f7f6e7b36c92c39c730e6b8c719b52
Jeff Wong <jeffmjwong@gmail.com>
2018-03-29 14:21:13 +1000

    Users can now add comments, edit and delete them but not comments by other users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :check_owner, only: [:edit, :update, :destroy]
 
   # GET /comments
   # GET /comments.json
@@ -20,7 +21,7 @@ class CommentsController < ApplicationController
 
   # GET /comments/1/edit
   def edit
-    @instapost = Instapost.find(params[:id])
+    @instapost = @comment.instapost
   end
 
   # POST /comments
@@ -45,7 +46,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to instapost_path({ id: comment_params[:instapost_id] }), notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -57,9 +58,10 @@ class CommentsController < ApplicationController
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
+    @instapost = @comment.instapost
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to @instapost, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -68,6 +70,12 @@ class CommentsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
+    end
+
+    def check_owner
+      if @comment.user != current_user
+        redirect_to(instaposts_url, notice: "You cannot perform that action!")
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :instaposts
-  resources(:comments, except: [:index, :new, :edit])
+  resources(:comments, except: [:index, :new])
   root to: "instaposts#index"
-  get '/instaposts/:id/comment/:id/edit', to: 'comments#edit', as: 'edit_comment'
+  # get '/instaposts/:id/comment/:id/edit', to: 'comments#edit', as: 'edit_comment'
   get '/instaposts/:id/comment', to: 'comments#new', as: 'new_comment'
 end
