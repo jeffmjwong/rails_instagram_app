41cc4d4ee33eb2d69a9a1442905f52fa9397bde0
Jeff Wong <jeffmjwong@gmail.com>
2018-03-29 13:26:38 +1000

    Set dependent: :delete_all in instapost model to delete all comments when delete a instapost

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,7 @@ class CommentsController < ApplicationController
 
   # GET /comments/1/edit
   def edit
+    @instapost = Instapost.find(params[:id])
   end
 
   # POST /comments
--- app/models/instapost.rb
+++ app/models/instapost.rb
@@ -1,6 +1,6 @@
 class Instapost < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :delete_all
   mount_uploader :photo, PhotoUploader
   validates :caption, presence: true
   validates :photo, presence: true
