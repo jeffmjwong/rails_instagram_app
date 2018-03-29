19be59a6e1b430f79ec6121b8582ae0b8d6d5188
Jeff Wong <jeffmjwong@gmail.com>
2018-03-29 12:04:54 +1000

    Allow users to comment on instaposts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,7 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def new
     @comment = Comment.new
+    @instapost = Instapost.find(params[:id])
   end
 
   # GET /comments/1/edit
@@ -25,10 +26,11 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
+    @comment.user = current_user
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to instapost_path({ id: comment_params[:instapost_id] }), notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :instaposts
-  resources(:comments, except: [:index])
+  resources(:comments, except: [:index, :new, :edit])
   root to: "instaposts#index"
+  get '/instaposts/:id/comment', to: 'comments#new', as: 'new_comment'
+  get '/instaposts/:id/comment/edit', to: 'comments#new', as: 'edit_comment'
 end
