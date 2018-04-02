97100f21a143d3b721908d7fb4d57ca40c2a4e68
Jeff Wong <jeffmjwong@gmail.com>
2018-04-02 13:34:03 +1000

    Generate model Comment with references to user and instapost

--- app/models/instapost.rb
+++ app/models/instapost.rb
@@ -1,6 +1,7 @@
 class Instapost < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
+  has_many :comments
   validates :caption, presence: true
   validates :photo, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328091426) do
+ActiveRecord::Schema.define(version: 20180402033047) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "instapost_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instapost_id"], name: "index_comments_on_instapost_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "instaposts", force: :cascade do |t|
     t.string "caption"
