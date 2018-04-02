238122d1afbbcc6dddc2841fb15de31c46b553f7
Jeff Wong <jeffmjwong@gmail.com>
2018-04-02 17:22:24 +1000

    Fix bug that doesn't let users to delete instaposts that have comments in it

--- app/models/instapost.rb
+++ app/models/instapost.rb
@@ -1,7 +1,7 @@
 class Instapost < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :caption, presence: true
   validates :photo, presence: true
 end
