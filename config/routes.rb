82d3a907c15be07f6951ea52e51be3effa61b6a2
Jeff Wong <jeffmjwong@gmail.com>
2018-04-02 16:57:10 +1000

    Refactor code and comments feature is implemented successfully

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "instaposts#index"
   resources :instaposts do
-    resources :comments
+    resources :comments, only: [:create, :edit, :update, :destroy]
   end
 end
