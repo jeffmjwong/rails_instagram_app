ed0abfe00b8ea03e08d09ddb68bc4c10e4ea6145
Jeff Wong <jeffmjwong@gmail.com>
2018-04-02 15:55:32 +1000

    Users can now type in comments in instapost show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,24 @@
 class CommentsController < ApplicationController
-  def new
+  before_action :set_instapost
 
+
+  def create
+    @comment = @instapost.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to @instapost
   end
+
+
+
+
+  private
+
+    def set_instapost
+      @instapost = Instapost.find(params[:instapost_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
