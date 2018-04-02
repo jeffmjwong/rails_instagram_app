d5520c40f4a687145492324346f2b82fa752605d
Jeff Wong <jeffmjwong@gmail.com>
2018-04-02 18:08:04 +1000

    Add validation to comment field and update error messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,25 +3,42 @@ class CommentsController < ApplicationController
   before_action :set_comment, except: [:create]
   before_action :check_owner, except: [:create]
 
+  def edit
+  end
+
   def create
     @comment = @instapost.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
-    redirect_to @instapost
-  end
 
-  def edit
-
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @instapost, notice: 'Comment added.' }
+        format.json { render :show, status: :created, location: @instapost }
+      else
+        format.html { redirect_to @instapost, notice: 'Comment cannot be empty.' }
+        format.json { render json: @instapost.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    @comment.update(comment_params)
-    redirect_to @instapost
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @instapost, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @instapost }
+      else
+        format.html { redirect_to edit_instapost_comment_path, notice: 'Comment cannot be empty.' }
+        format.json { render json: @instapost.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @comment.destroy
-    redirect_to @instapost
+    respond_to do |format|
+      format.html { redirect_to @instapost, notice: 'Comment was removed.' }
+      format.json { head :no_content }
+    end
   end
 
 
