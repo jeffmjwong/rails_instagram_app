e4dc001aeb7a00aa55f776e3b7537c8a4934d033
Jeff Wong <jeffmjwong@gmail.com>
2018-04-02 16:46:01 +1000

    User can edit and delete their own comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_instapost
-
+  before_action :set_comment, except: [:create]
+  before_action :check_owner, except: [:create]
 
   def create
     @comment = @instapost.comments.new(comment_params)
@@ -9,13 +10,35 @@ class CommentsController < ApplicationController
     redirect_to @instapost
   end
 
+  def edit
 
+  end
+
+  def update
+    @comment.update(comment_params)
+    redirect_to @instapost
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @instapost
+  end
 
 
   private
 
     def set_instapost
       @instapost = Instapost.find(params[:instapost_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def check_owner
+      if @comment.user != current_user
+        redirect_to @instapost, notice: 'You are not permitted to perform that action!'
+      end
     end
 
     def comment_params
